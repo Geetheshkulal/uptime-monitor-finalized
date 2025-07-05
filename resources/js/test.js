b77726a00e64f2231b31fef1c26b7b695f1173f0
Sachin Chatterjee <sachinchatterjee31796@gmail.com>
2025-07-05 10:30:45 +0530

    reverb setup changes a

--- resources/js/test.js
+++ resources/js/test.js
@@ -7,15 +7,27 @@ console.log("✅ test.js is loaded");
 
 window.Pusher = Pusher;
 
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
+//     wsPort: Number(import.meta.env.VITE_REVERB_PORT) || 8080,
+//     wssPort: Number(import.meta.env.VITE_REVERB_PORT) || 8080,
+//     forceTLS: false,
+//     disableStats: true,
+//     enabledTransports: ['ws'],
+// });
+
 window.Echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST || window.location.hostname,
-    wsPort: Number(import.meta.env.VITE_REVERB_PORT) || 8080,
-    wssPort: Number(import.meta.env.VITE_REVERB_PORT) || 8080,
-    forceTLS: false,
-    disableStats: true,
-    enabledTransports: ['ws'],
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: window.location.hostname,
+  wsPort: 443,
+  wssPort: 443,
+  wsPath: '/reverb',
+  forceTLS: true,
+  disableStats: true,
+  enabledTransports: ['ws'],
 });
 
 window.Echo.connector.pusher.connection.bind('connected', () => {
