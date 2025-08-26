3c938e1c531726bfb898c4ca872535b566771116
geethesh kulal <geetheshkulal84@gmail.com>
2025-08-26 16:35:37 +0530

    26-08-2025

--- resources/views/TermsAndConditions/cookies.blade.php
+++ resources/views/TermsAndConditions/cookies.blade.php
@@ -227,12 +227,12 @@
         <div class="container terms-content mt-5">
             <h1 class="mb-3">Cookies Policy</h1>
             <p>Last updated: August 26, 2025</p>
-            <p>This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used. This Cookies Policy has been created with the help of the <a href="https://www.termsfeed.com/cookies-policy-generator/" target="_blank">Cookies Policy Generator</a>.</p>
+            <p>This Cookies Policy explains what Cookies are and how We use them. You should read this policy so You can understand what type of cookies We use, or the information We collect using Cookies and how that information is used.</p>
             <p>Cookies do not typically contain any information that personally identifies a user, but personal information that we store about You may be linked to the information stored in and obtained from Cookies. For further information on how We use, store and keep your personal data secure, see our Privacy Policy.</p>
             <p>We do not store sensitive personal information, such as mailing addresses, account passwords, etc. in the Cookies We use.</p>
             <h2>Interpretation and Definitions</h2>
             <h3>Interpretation</h3>
-            <p>The words of which the initial letter is capitalized have meanings defined under the following conditions. The following definitions shall have the same meaning regardless of whether they appear in singular or in plural.</p>
+            <p>The words with their initial letter capitalized have specific meanings as defined below. These definitions apply whether the terms appear in singular or plural..</p>
             <h3>Definitions</h3>
             <p>For the purposes of this Cookies Policy:</p>
             <ul>
@@ -244,19 +244,19 @@
             <h2>The use of the Cookies</h2>
             <h3>Type of Cookies We Use</h3>
             <p>Cookies can be &quot;Persistent&quot; or &quot;Session&quot; Cookies. Persistent Cookies remain on your personal computer or mobile device when You go offline, while Session Cookies are deleted as soon as You close your web browser.</p>
-            <p>We use both session and persistent Cookies for the purposes set out below:</p>
+            <p>We use a combination of session and persistent cookies for different purposes, including development and security:</p>
             <ul>
             <li>
             <p><strong>Necessary / Essential Cookies</strong></p>
             <p>Type: Session Cookies</p>
-            <p>Administered by: Us</p>
+            <p>Examples: session, XSRF-TOKEN, PHPSESSID</p>
             <p>Purpose: These Cookies are essential to provide You with services available through the Website and to enable You to use some of its features. They help to authenticate users and prevent fraudulent use of user accounts. Without these Cookies, the services that You have asked for cannot be provided, and We only use these Cookies to provide You with those services.</p>
             </li>
             <li>
-            <p><strong>Functionality Cookies</strong></p>
+            <p><strong>Analytics Cookies</strong></p>
             <p>Type: Persistent Cookies</p>
-            <p>Administered by: Us</p>
-            <p>Purpose: These Cookies allow us to remember choices You make when You use the Website, such as remembering your login details or language preference. The purpose of these Cookies is to provide You with a more personal experience and to avoid You having to re-enter your preferences every time You use the Website.</p>
+            <p>Purpose:Monitor website traffic and performance.Help improve user experience by tracking user behavior anonymously.
+            Behavior: These cookies stay in your browser until they expire or are manually cleared.</p>
             </li>
             </ul>
             <h3>Your Choices Regarding Cookies</h3>
